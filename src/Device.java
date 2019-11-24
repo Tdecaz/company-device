a83b7d50881b22e2cfa0aa2b080135c33bdf3026
„Tdecazgit config --global user.email „tadas.kaluina@gmail.com <tadas.kaluina@gmail.com>
2019-11-24 11:18:48 +0200

    added classes

---
+++ src/Device.java
@@ -0,0 +1,24 @@
+public class Device {
+
+private String name;
+
+    private String address;
+    private String productionType;
+    private String devices;
+
+    Device (String address, String productionType, String devices){
+
+        this.address = address;
+        this.productionType = productionType;
+        this.devices = devices;
+    }
+
+    public String getaddress() { return address; }
+    public String productionType() { return productionType; }
+    public String devices() { return devices; }
+
+}
+
+
+
+
