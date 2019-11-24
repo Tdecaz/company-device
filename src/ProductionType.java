abefce870d7301482f8c3057dfb5d870d0e40a8e
DainaLa <dainamaz@gmail.com>
2019-11-24 12:41:14 +0200

    add production type

--- src/ProductionType.java
+++ src/ProductionType.java
@@ -1,2 +1,19 @@
-public enum  ProductionType {
-}
+     public enum ProductionType {
+        TECHNOLOGIES(""),
+        AUTOMOBILES(""),
+        GARDEN_TOOLS(""),
+        FOOD("");
+
+        private String productionDescription;
+
+        ProductionType(String productionDescription) {
+            this.productionDescription = productionDescription;
+        }
+
+        public String getTriangleDescription() {
+            return productionDescription;
+        }
+    }
+
+
+
